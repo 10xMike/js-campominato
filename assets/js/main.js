c32e73ee6b8a42a591d47831c629b24c26c85176
10xMike <michelegec@gmail.com>
2021-05-09 20:02:12 +0200

    Commit iniziale

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Chiedo all'utente di scegliere la difficoltà e controllo che abbia inserito un valore giusto
+// In base alla difficoltà vado a decidere la quantità di numeri
+// Il computer deve generare 16 numeri casuali tra 1 e 100
+// I numeri non possono essere duplicati
+// In seguito devo chiedere all'utente (100 - 16) volte di inserire un numero alla volta, compreso tra 1 e 100
+// L'utente non può inserire più volte lo stesso numero
+// Comunico all'utente se ha vinto
